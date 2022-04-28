d78f762aa6cb5a30ac2e43bc67a1b36c21f21754
unknown <dgloria.web@gmail.com>
2022-04-28 11:26:27 +0100

    first

---
+++ src/views/HomeView.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+    <h1>Made By Getters</h1>
+  <div v-for='gettersUser in getUsers' :key='gettersUser.id'>
+    {{gettersUser.id}} {{gettersUser.name}} {{gettersUser.address}}
+    </div>
+    <h1>Made By Actions</h1>
+  <div v-for='user in users' :key='user.id'>
+    {{user.id}} {{user.name}} {{user.address}}
+    </div>
+  </div>
+</template>
+
+<script setup>
+import { ref, onMounted, computed } from 'vue';
+//import users store
+import { useUserStore } from "../store/index";
+// declare store variable
+const store = useUserStore();
+
+const msg = ref("Welcome to my Vuex Store");
+
+const getUsers = computed(() => {
+  return store.getUsers
+})
+const users = computed(() => {
+  return store.users
+})
+
+onMounted(() => {
+  store.fetchUsers();
+  store.checkLoggedInStatus();
+})
+</script>
