3fef0c20c0ba1e2f15d3fdd19751c59ee5c278d1
unknown <dgloria.web@gmail.com>
2022-04-30 10:04:24 +0100

    presenting match data

--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -1,8 +1,20 @@
 <template>
   <div class="">
-    <h1>List of Recommended Jobs</h1>
-
-    {{ jobStore.match_rates }}
+        <div id="myGridColumn" v-if="jobStore.match_rates">
+      <h2>Your Personal Preferences</h2>
+      <div id="ratesContainer">
+        <div v-for="job in jobStore.match_rates" :key="job.id" id="myGridRow">
+          <div class="grid-cell-1">
+            <router-link :to="{ name: 'jobDetail', params: { id: job.id } }">{{
+              job.job_name
+            }}</router-link>
+          </div>
+          <div class="grid-cell-2">
+            {{ job.job_rate }}
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
