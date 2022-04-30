d6486700f733165cfea12316f32a55b374eaf2e9
unknown <dgloria.web@gmail.com>
2022-04-30 10:19:42 +0100

    presenting data from store

--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -1,6 +1,6 @@
 <template>
-  <div class="">
-        <div id="myGridColumn" v-if="jobStore.match_rates">
+  <div id="myGridContainer">
+    <div id="myGridColumn" v-if="jobStore.match_rates">
       <h2>Your Personal Preferences</h2>
       <div id="ratesContainer">
         <div v-for="job in jobStore.match_rates" :key="job.id" id="myGridRow">
@@ -15,15 +15,180 @@
         </div>
       </div>
     </div>
+    <div id="myGridColumn" v-if="jobStore.personUpdated">
+      <h2>List of your preferences/settings</h2>
+
+      <div id="settingsContainer">
+        <div class="grid-cell-1">
+          <h3>Usual work location</h3>
+          
+          <input
+            :myClass="{ 'red-text': isDirty['workplace'] }"
+            type="checkbox"
+            name="workplace"
+            id="workplaceChk"
+            v-model="jobStore.personUpdated.workplace"
+          />
+          <label for="workplaceChk">I want to work at the workplace</label>
+          <br />
+          <input
+            :myClass="{ 'red-text': isDirty['remote'] }"
+            type="checkbox"
+            name="remote"
+            id="remoteChk"
+            v-model="jobStore.personUpdated.remote"
+          />
+          <label for="remoteChk">I want to work remotely</label>
+          <br />
+          <h3>Usual working days</h3>
+          <input
+            :myClass="{ 'red-text': isDirty['workdays'] }"
+            type="checkbox"
+            name="workdays"
+            id="workdaysChk"
+            v-model="jobStore.personUpdated.workdays"
+          />
+          <label for="workdaysChk">I want to work workdays</label>
+          <br />
+          <input
+            :myClass="{ 'red-text': isDirty['saturday'] }"
+            type="checkbox"
+            name="saturday"
+            id="saturdayChk"
+            v-model="jobStore.personUpdated.saturday"
+          />
+          <label for="saturdayChk">Can do Saturdays</label>
+          <br />
+          <input
+            :myClass="{ 'red-text': isDirty['sunday'] }"
+            type="checkbox"
+            name="sunday"
+            id="sundayChk"
+            v-model="jobStore.personUpdated.sunday"
+          />
+          <label for="sundayChk">Can do Sundays</label>
+          <br />
+          <input
+            :myClass="{ 'red-text': isDirty['bank_holidays'] }"
+            type="checkbox"
+            name="bank_holidays"
+            id="bank_holidaysChk"
+            v-model="jobStore.personUpdated.bank_holidays"
+          />
+          <label for="bank_holidaysChk">Can do bank holidays</label>
+          <br />
+
+          <h4>Special working days</h4>
+          <input
+            :myClass="{ 'red-text': isDirty['sat_sun_bh_only'] }"
+            type="checkbox"
+            name="sat_sun_bh_only"
+            id="sat_sun_bh_onlyChk"
+            v-model="jobStore.personUpdated.sat_sun_bh_only"
+          />
+          <label for="sat_sun_bh_onlyChk"
+            >want to work only Saturdays, Sundays and Bank holidays</label
+          >
+          <br />
+
+          <h3>Usual working hours</h3>
+          <input
+            :myClass="{ 'red-text': isDirty['normal_hours'] }"
+            type="checkbox"
+            name="normal_hours"
+            id="normal_hoursChk"
+            v-model="jobStore.personUpdated.normal_hours"
+          />
+          <label for="normal_hoursChk">Want to work normal hours</label>
+          <br />
+          <input
+            :myClass="{ 'red-text': isDirty['nightshift'] }"
+            type="checkbox"
+            name="nightshift"
+            id="nightshiftChk"
+            v-model="jobStore.personUpdated.nightshift"
+          />
+          <label for="nightshiftChk">Can do nightshift</label>
+          <br />
+          <input
+            :myClass="{ 'red-text': isDirty['other_shift'] }"
+            type="checkbox"
+            name="other_shift"
+            id="other_shiftChk"
+            v-model="jobStore.personUpdated.other_shift"
+          />
+          <label for="other_shiftChk">Can do other shift</label>
+          <br />
+
+          <h4>Special working hours</h4>
+          <input
+            :myClass="{ 'red-text': isDirty['nightshift_only'] }"
+            type="checkbox"
+            name="nightshift_only"
+            id="nightshift_onlyChk"
+            v-model="jobStore.personUpdated.nightshift_only"
+          />
+          <label for="nightshift_onlyChk">Want to do nightshift only</label>
+          <br />
+          <input
+            :myClass="{ 'red-text': isDirty['other_shift_only'] }"
+            type="checkbox"
+            name="other_shift_only"
+            id="other_shift_onlyChk"
+            v-model="jobStore.personUpdated.other_shift_only"
+          />
+          <label for="other_shift_onlyChk"
+            >Want to do ther shift only (eg. late afternoons)</label
+          >
+          <br />
+          <h3>Other</h3>
+          <input
+            :myClass="{ 'red-text': isDirty['overtime'] }"
+            type="checkbox"
+            name="overtime"
+            id="overtimeChk"
+            v-model="jobStore.personUpdated.overtime"
+          />
+          <label for="overtimeChk">Want to do jobs that has overtime</label>
+          <br />
+        </div>
+
+        <button @click="confirmPersonSettingChanges()">Store settings</button>
+      </div>
+    </div>
   </div>
+
 </template>
 
 <script setup>
 import { useJobStore } from '../store/jobstore'
 const jobStore = useJobStore()
-
+var isDirty = []
 
 </script>
 
 <style scoped>
+#myGridContainer {
+  width: fit-content;
+  display: flex;
+  gap: 35px;
+}
+#myGridColumn {
+  padding: 15px;
+}
+#myGridRow {
+  display: grid;
+  grid-template-columns: 3fr 1fr;
+  padding: 10px;
+}
+
+.grid-cell-1 {
+  width: max-content;
+}
+.grid-cell-2 {
+  text-align: right;
+}
+.grid-cell-2:after {
+  content: "%";
+}
 </style>
