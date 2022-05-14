744633f9f66303a3db8f5fcf3e76bd8bcd6feee8
unknown <dgloria.web@gmail.com>
2022-05-14 22:59:11 +0100

    created the first working component with props and emits

--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -141,64 +141,38 @@
         </div>
 
         <br />
-        <button @click="showModal('changes: abc')">Store settings</button>
+        <button id="show-modal" @click="showModalFunc">Store settings</button>
       </div>
     </div>
   </div>
-  
-  <Transition name="modal">
-    <div v-if="show" class="modal-mask">
-      <div class="modal-wrapper">
-        <div class="modal-container">
-          <div class="modal-header">
-            <slot name="header">default header</slot>
-          </div>
-          {{ text }}
-          <div class="modal-body">
-            <slot name="body">default body</slot>
-          </div>
-
-          <div class="modal-footer">
-            <slot name="footer">
-              default footer
-              <button
-                class="modal-default-button"
-                @click="confirmPersonSettingChanges()"
-              >
-                OK
-              </button>
-              <button class="modal-default-button" @click="closeModal">
-                Cancel
-              </button>
-            </slot>
-          </div>
-        </div>
-      </div>
-    </div>
-  </Transition>
+  <ModalComponent
+    :show="showModal"
+    @close="hideModal"
+    @confirmPersonSettingChanges="confirmPersonSettingChanges()"
+  />
 </template>
 
 <script setup>
 import { useJobStore } from '../store/jobstore'
 import personService from '../services/person.service'
+import ModalComponent from '@/components/ModalComponent'
 import { ref } from 'vue'
+
 const jobStore = useJobStore()
 
-let show = ref(false)
-let text = ref('')
+let showModal = ref(false)
 
-function showModal(submittedText) {
-  text = submittedText
-  show.value = true
+function hideModal() {
+  showModal.value = false
 }
-function closeModal() {
-  show.value = false
+function showModalFunc() {
+  showModal.value = true
 }
-
 function confirmPersonSettingChanges() {
   // validation here
   storePersonData()
 }
+
 
 function storePersonData() {
   personService.updatePerson(jobStore.personUpdated).then(
@@ -211,11 +185,9 @@ function storePersonData() {
       }
       else {
         alert('settings updated')
-        closeModal()
-
+        hideModal()
       }
       jobStore.setJobSummaryData() // this might not even be required...
-
     })
     .catch(error => {
       if (error.response.data.errors) {
@@ -251,59 +223,5 @@ function storePersonData() {
 }
 .grid-cell-2:after {
   content: "%";
-}
-
-.modal-mask {
-  position: fixed;
-  z-index: 9998;
-  top: 0;
-  left: 0;
-  width: 100%;
-  height: 100%;
-  background-color: rgba(0, 0, 0, 0.5);
-  display: table;
-  transition: opacity 0.3s ease;
-}
-
-.modal-wrapper {
-  display: table-cell;
-  vertical-align: middle;
-}
-
-.modal-container {
-  width: 300px;
-  margin: 0px auto;
-  padding: 20px 30px;
-  background-color: #fff;
-  border-radius: 2px;
-  box-shadow: 0 2px 8px rgba(0, 0, 0, 0.33);
-  transition: all 0.3s ease;
-}
-
-.modal-header h3 {
-  margin-top: 0;
-  color: #42b983;
-}
-
-.modal-body {
-  margin: 20px 0;
-}
-
-.modal-default-button {
-  float: right;
-}
-
-.modal-enter-from {
-  opacity: 0;
-}
-
-.modal-leave-to {
-  opacity: 0;
-}
-
-.modal-enter-from .modal-container,
-.modal-leave-to .modal-container {
-  -webkit-transform: scale(1.1);
-  transform: scale(1.1);
 }
 </style>
