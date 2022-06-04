5f653310e2176ff91adf0dac3ff8991386fca641
unknown <dgloria.web@gmail.com>
2022-06-04 15:11:05 +0100

    show all skills

--- src/store/skillstore.js
+++ src/store/skillstore.js
@@ -1,18 +1,22 @@
 //stores/users.js
 
 import personService from '@/services/person.service'
-import { defineStore } from 'pinia'
+import { defineStore, setMapStoreSuffix } from 'pinia'
 // import personService from '@/services/person.service';
 
 // always rename the defineStore because it can cause disambiguity if names are the same
 export const useSkillStore = defineStore('skills', {
   state: () => ({
     skill: null,
+    skills: null,
   }),
   getters: {
     getSkill(state) {
       return state.skill
     },
+    getSkills(state){
+      return state.skills
+    }
   },
   actions: {
     setSkill(){
