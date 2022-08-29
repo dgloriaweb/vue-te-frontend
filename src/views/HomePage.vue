527f0f46f5c8f66f2769fb463481a3d3e9e08ec9
unknown <dgloria.web@gmail.com>
2022-08-29 11:12:08 +0100

    added scenarios to frontend to reduce backend requests

--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -33,6 +33,7 @@
           name="workplace"
           id="workplaceChk"
           v-model="jobStore.personUpdated.workplace"
+          @change="checkWorkplace"
         />
         <label for="workplaceChk">I want to work at the workplace</label>
         <br />
@@ -42,6 +43,7 @@
           name="remote"
           id="remoteChk"
           v-model="jobStore.personUpdated.remote"
+          @change="checkRemote"
         />
         <label for="remoteChk">I want to work remotely</label>
         <br />
@@ -53,6 +55,7 @@
           name="overtime"
           id="overtimeChk"
           v-model="jobStore.personUpdated.overtime"
+          @change="checkOvertime"
         />
         <label for="overtimeChk">Want to do jobs that has overtime</label>
       </div>
@@ -64,6 +67,7 @@
           name="workdays"
           id="workdaysChk"
           v-model="jobStore.personUpdated.workdays"
+          @change="checkWorkdays"
         />
         <label for="workdaysChk">I want to work workdays</label>
         <br />
@@ -73,6 +77,7 @@
           name="saturday"
           id="saturdayChk"
           v-model="jobStore.personUpdated.saturday"
+          @change="checkSaturday"
         />
         <label for="saturdayChk">Can do Saturdays</label>
         <br />
@@ -82,6 +87,7 @@
           name="sunday"
           id="sundayChk"
           v-model="jobStore.personUpdated.sunday"
+          @change="checkSunday"
         />
         <label for="sundayChk">Can do Sundays</label>
         <br />
@@ -91,6 +97,7 @@
           name="bank_holidays"
           id="bank_holidaysChk"
           v-model="jobStore.personUpdated.bank_holidays"
+          @change="checkBankholidays"
         />
         <label for="bank_holidaysChk">Can do bank holidays</label>
         <br />
@@ -101,6 +108,7 @@
           name="sat_sun_bh_only"
           id="sat_sun_bh_onlyChk"
           v-model="jobStore.personUpdated.sat_sun_bh_only"
+          @change="checkSatsunbhonly"
         />
         <label for="sat_sun_bh_onlyChk"
           >want to work only Saturdays, Sundays and Bank holidays</label
@@ -115,6 +123,7 @@
           name="normal_hours"
           id="normal_hoursChk"
           v-model="jobStore.personUpdated.normal_hours"
+          @change="checkNormalhours"
         />
         <label for="normal_hoursChk">Want to work normal hours</label>
         <br />
@@ -124,6 +133,7 @@
           name="nightshift"
           id="nightshiftChk"
           v-model="jobStore.personUpdated.nightshift"
+          @change="checkNightshift"
         />
         <label for="nightshiftChk">Can do nightshift</label>
         <br />
@@ -133,6 +143,7 @@
           name="other_shift"
           id="other_shiftChk"
           v-model="jobStore.personUpdated.other_shift"
+          @change="checkOthershift"
         />
         <label for="other_shiftChk">Can do other shift</label>
         <br />
@@ -144,6 +155,7 @@
           name="nightshift_only"
           id="nightshift_onlyChk"
           v-model="jobStore.personUpdated.nightshift_only"
+          @change="checkNightshiftonly"
         />
         <label for="nightshift_onlyChk">Want to do nightshift only</label>
         <br />
@@ -153,6 +165,7 @@
           name="other_shift_only"
           id="other_shift_onlyChk"
           v-model="jobStore.personUpdated.other_shift_only"
+          @change="checkOthershiftonly"
         />
         <label for="other_shift_onlyChk"
           >Want to do other shift only (eg. late afternoons)</label
@@ -228,6 +241,133 @@ function storePersonData() {
     })
 }
 
+/* *******************************************************
+// investigate cases of checkboxes workplace and remote
+**********************************************************/
+
+function checkWorkplace(e) {
+  //console.log(e.target.checked); //tells if the checkbox is selected or not
+  if (e.target.checked) {// uncheck checkRemote
+    jobStore.personUpdated.remote = false
+  }
+}
+function checkRemote(e) {
+  if (e.target.checked) {
+    jobStore.personUpdated.workplace = false
+  }
+}
+
+/* *******************************************************
+// investigate cases of checkboxes days
+**********************************************************/
+function checkSatsunbhonly(e) {
+  if (e.target.checked) {
+    jobStore.personUpdated.workdays = false
+    jobStore.personUpdated.saturday = false
+    jobStore.personUpdated.sunday = false
+    jobStore.personUpdated.bank_holidays = false
+  } else {
+    jobStore.personUpdated.workdays = true
+  }
+}
+function checkWorkdays(e) {
+  if (e.target.checked) {
+    jobStore.personUpdated.sat_sun_bh_only = false
+  } else {
+    if (jobStore.personUpdated.saturday == 1 && jobStore.personUpdated.sunday == 1 && jobStore.personUpdated.bank_holidays == 1) {
+      jobStore.personUpdated.saturday = false
+      jobStore.personUpdated.sunday = false
+      jobStore.personUpdated.bank_holidays = false
+      jobStore.personUpdated.sat_sun_bh_only = true
+    }
+  }
+}
+function checkSaturday(e) {
+  if (e.target.checked) {
+    if (jobStore.personUpdated.workdays == false && jobStore.personUpdated.sunday == 1 && jobStore.personUpdated.bank_holidays == 1) {
+      jobStore.personUpdated.saturday = false
+      jobStore.personUpdated.sunday = false
+      jobStore.personUpdated.bank_holidays = false
+      jobStore.personUpdated.sat_sun_bh_only = true
+    } else {
+      jobStore.personUpdated.sat_sun_bh_only = false
+    }
+  }
+}
+function checkSunday(e) {
+  if (e.target.checked) {
+    if (jobStore.personUpdated.workdays == false && jobStore.personUpdated.saturday == 1 && jobStore.personUpdated.bank_holidays == 1) {
+      jobStore.personUpdated.saturday = false
+      jobStore.personUpdated.sunday = false
+      jobStore.personUpdated.bank_holidays = false
+      jobStore.personUpdated.sat_sun_bh_only = true
+    } else {
+      jobStore.personUpdated.sat_sun_bh_only = false
+    }
+  }
+}
+function checkBankholidays(e) {
+  if (e.target.checked) {// uncheck checkRemote
+    if (jobStore.personUpdated.workdays == false && jobStore.personUpdated.saturday == 1 && jobStore.personUpdated.sunday == 1) {
+      jobStore.personUpdated.saturday = false
+      jobStore.personUpdated.sunday = false
+      jobStore.personUpdated.bank_holidays = false
+      jobStore.personUpdated.sat_sun_bh_only = true
+    } else {
+      jobStore.personUpdated.sat_sun_bh_only = false
+    }
+  }
+}
+/* *******************************************************
+// investigate cases of checkboxes hours
+**********************************************************/
+function checkNormalhours(e) {
+  if (e.target.checked) {
+    jobStore.personUpdated.nightshift_only = false
+    jobStore.personUpdated.other_shift_only = false
+  } else if(jobStore.personUpdated.other_shift_only == 0){
+    jobStore.personUpdated.nightshift = true
+  }
+}
+function checkNightshiftonly(e) {
+  if (e.target.checked) {
+    jobStore.personUpdated.normal_hours = false
+    jobStore.personUpdated.nightshift = true
+    jobStore.personUpdated.other_shift = false
+    jobStore.personUpdated.other_shift_only = false
+  }
+}
+function checkOthershift(e) {
+  if (e.target.checked) {
+    jobStore.personUpdated.nightshift_only = false
+    if (jobStore.personUpdated.normal_hours == 0 && jobStore.personUpdated.nightshift == 0) {
+      jobStore.personUpdated.other_shift_only = true
+    }
+
+  } else if (jobStore.personUpdated.nightshift == 1) {
+    jobStore.personUpdated.nightshift_only = true
+  }
+}
+function checkOthershiftonly(e) {
+  if (e.target.checked) {
+    jobStore.personUpdated.other_shift = true
+    jobStore.personUpdated.nightshift = false
+    jobStore.personUpdated.nightshift_only = false
+    jobStore.personUpdated.normal_hours = false
+  }
+}
+function checkNightshift(e) {
+  if (e.target.checked) {
+    jobStore.personUpdated.other_shift_only = false
+    if (jobStore.personUpdated.normal_hours == 0 && jobStore.personUpdated.other_shift == 0) {
+      jobStore.personUpdated.nightshift_only = true
+    }
+  } else if (jobStore.personUpdated.other_shift == 1) {
+    jobStore.personUpdated.other_shift_only = true
+  } else if (jobStore.personUpdated.normal_hours == 1){
+    jobStore.personUpdated.nightshift_only = false
+  }
+}
 
 </script>
 
