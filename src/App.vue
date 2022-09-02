79a0f1e134810bee6716fbc81fbd9805c1823aca
unknown <dgloria.web@gmail.com>
2022-09-02 10:17:50 +0100

    added footer

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,18 @@
 <template>
   <myheader />
   <router-view />
+  <myfooter/>
 </template>
 
 <script>
 import myheader from "./components/MyHeader.vue"
+import myfooter from "./components/MyFooter.vue"
 
 
 export default {
   components: {
-    myheader
+    myheader,
+    myfooter
   }
 }
 </script>
---
+++ src/components/MyFooter.vue
@@ -0,0 +1,13 @@
+<template>
+    <div id="footer">
+        <i>Created and maintained by <a href="https://github.com/dgloriaweb">@DGloriaWeb</a></i>
+    </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style  scoped>
+
+</style>
