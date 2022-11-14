51697a0d7d68e58cb4dc7dba65c62087d1e17b31
Catalin Muresian <catalinmuresian@yahoo.com>
2022-11-14 15:44:03 +0200

    store-edit

--- src/store/jobstore.js
+++ src/store/jobstore.js
@@ -7,12 +7,7 @@ import jobService from '@/services/job.service'
 export const useJobStore = defineStore('jobs', {
   state: () => ({
     match_rates: null,
-    jobUpdated: {
-      workdays: null   // u have to declare workdays, because u receive error of undefined when the page is rendered
-    },
-    personUpdated: {
-      workdays: null
-    },
+    personUpdated: null,
     job: null,
   }),
   getters: {
